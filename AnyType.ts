88239e9e1c4ff0c7c7467fa1fa508d78bf6d72d3
Nilesh kumar <kumarnilesh843127@gmail.com>
2024-10-16 20:22:38 +0530

    first commit

---
+++ AnyType.ts
@@ -0,0 +1,23 @@
+ 
+//  TypeScript has a special any type that can be used to represent any type. When a variable is annotated with any type .typescript will allow it to have any value and disable all type checking for that variable and its properties.
+
+
+
+
+
+
+// let  Color : any = "string";
+// Color = 5;
+// console.log(Color);
+
+// Color = "Nilesh Kumar"
+// console.log(Color);
+
+
+
+let  Color : string  = "string";
+// Color = 5;
+console.log(Color);
+
+Color = "Nilesh Kumar"
+console.log(Color);
---
+++ index.ts
@@ -0,0 +1,42 @@
+// // Annotation : 
+
+// //  used to specify the data type of a variable parameter, function return value and other type of values ..
+
+
+
+
+// //============---------            String 
+
+
+
+
+
+// let myName :string = "Nilesh kumar";
+// myName ="Sreyansh";
+// console.log(myName);
+
+// let person : string = "Huxn";
+// console.log(person);
+
+
+
+// //---------Number
+
+// let number : Number = 8;
+// console.log(number);
+
+// // boolean
+
+// let isTSHard : boolean = true;
+// console.log(isTSHard);
+
+
+
+
+
+// let tech = 'Typescipt'
+// console.log(typeof (tech));
+// let favNumber = 8;
+// console.log(favNumber);
+
+
