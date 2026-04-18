3c00481d7c345665d53597be43b722cebff6a796
Nilesh Kumar <kumarnilesh843127@gmail.com>
2026-04-18 13:13:20 +0530

    first commit

---
+++ Array.ts
@@ -0,0 +1,23 @@
+
+// const names: string[] = ["Java", "Spring boot", "typescript"];
+
+
+// for(let a of names){
+//     console.log(a);
+
+// }
+
+
+
+// Tuples array : It is also the types array and in this the types of the each index of the array element is mentioned
+
+
+
+let arr : [string, number, boolean];
+
+arr = ["nilesh", 23, true];
+
+
+for(let x of arr){
+    console.log(x);
+}
---
+++ hello.ts
@@ -0,0 +1,10 @@
+// const GreetHello= (name :string)=>{
+//     console.log(`hello ${name}`)
+// }
+// GreetHello("Nilesh");
+
+
+// 
+let isActive :boolean  = false; 
+
+console.log(isActive);
