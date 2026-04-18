cc149d71aadd5b08604c192e3546b5ee2830607f
Nilesh Kumar <kumarnilesh843127@gmail.com>
2026-04-18 13:35:26 +0530

    add the typescrip on Object

---
+++ Object.ts
@@ -0,0 +1,19 @@
+const person: {
+  name: string;
+  email: string;
+  age: number;
+  skills :string[],
+} = {
+  name: "nilesh",
+  email: "nilesh@gmail.com",
+  age: 430,
+  skills :["java", "typescript", "spring boot"]
+};
+
+// console.log(person);
+
+console.log(person.skills);
+
+
+
+
