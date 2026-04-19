d7ba649f40b4f2be49e38cba15f4c1c3e03f2a4a
Nilesh Kumar <kumarnilesh843127@gmail.com>
2026-04-19 21:37:34 +0530

    Oops in typescript

--- Classes/Inheritance.ts
+++ Classes/Inheritance.ts
@@ -1,1 +1,31 @@
- 
+class Person {
+  firstname: string;
+   lastname: string;
+
+  constructor(firstname: string, lastname: string) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+    
+   greet (): string {
+     return `welcome ${this.firstname} ${this.lastname}`;
+   }
+}
+
+
+class student extends Person{
+
+     id : number;
+
+     constructor( firstname : string , lastname : string, id : number){
+        super(firstname, lastname);
+        this.id = id;
+     }
+
+     greet(): string {
+         return `welcome ${this.firstname} ${this.lastname}, this  is Your registration id : ${this.id}`;
+     }
+}
+
+const s = new student("Komal", "kr", 234324);
+console.log(s.greet());
--- Object.ts
+++ Object.ts
@@ -1,18 +1,18 @@
-const person: {
-  name: string;
-  email: string;
-  age: number;
-  skills :string[],
-} = {
-  name: "nilesh",
-  email: "nilesh@gmail.com",
-  age: 430,
-  skills :["java", "typescript", "spring boot"]
-};
+// const person: {
+//   name: string;
+//   email: string;
+//   age: number;
+//   skills :string[],
+// } = {
+//   name: "nilesh",
+//   email: "nilesh@gmail.com",
+//   age: 430,
+//   skills :["java", "typescript", "spring boot"]
+// };
 
-// console.log(person);
+// // console.log(person);
 
-console.log(person.skills);
+// console.log(person.skills);
 
 
 
